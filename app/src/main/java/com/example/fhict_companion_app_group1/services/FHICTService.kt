802bb4c841c786d24a35d040584444c22d2c760a
Justice Dreischor <t-justice98@hotmail.com>
2021-12-09 11:53:18 +0100

    handle exception when courses are not found

--- app/src/main/java/com/example/fhict_companion_app_group1/services/FHICTService.kt
+++ app/src/main/java/com/example/fhict_companion_app_group1/services/FHICTService.kt
@@ -8,6 +8,7 @@ import com.example.fhict_companion_app_group1.models.Token
 import com.google.gson.Gson
 import org.json.JSONArray
 import org.json.JSONObject
+import java.io.IOException
 import java.net.URL
 import java.util.*
 import kotlin.collections.ArrayList
@@ -27,15 +28,20 @@ class FHICTService(private val token: Token) {
         )
         httpsURLConnection.connect()
 
-        val inputStream = httpsURLConnection.getInputStream()
-        val scanner = Scanner(inputStream)
-        val jsonString = scanner.useDelimiter("\\Z").next()
-        val jsonObject = JSONObject(jsonString)
-        val jsonArray = jsonObject.getJSONArray("data")
+        try {
+            val inputStream = httpsURLConnection.getInputStream()
+            val scanner = Scanner(inputStream)
+            val jsonString = scanner.useDelimiter("\\Z").next()
+            val jsonObject = JSONObject(jsonString)
+            val jsonArray = jsonObject.getJSONArray("data")
 
-        for (i in 0 until jsonArray.length()) {
-            val innerJsonObject: String = jsonArray.get(i).toString()
-            courses.add(Gson().fromJson(innerJsonObject, Course::class.java))
+            for (i in 0 until jsonArray.length()) {
+                val innerJsonObject: String = jsonArray.get(i).toString()
+                courses.add(Gson().fromJson(innerJsonObject, Course::class.java))
+            }
+        } catch (ex: IOException) {
+            ex.printStackTrace()
+            return getMockCourses()
         }
         return courses
     }
@@ -88,7 +94,7 @@ class FHICTService(private val token: Token) {
 
         val jsonArray = JSONArray(jsonString)
 
-        for (i in 0 until 10) {
+        for (i in 0 until jsonArray.length()) {
             val innerJsonObject: String = jsonArray.get(i).toString()
             val info = Gson().fromJson(innerJsonObject, People::class.java)
             people.add(info)
@@ -116,4 +122,54 @@ class FHICTService(private val token: Token) {
         return Gson().fromJson(jsonObjectToString, People::class.java)
     }
 
+    // Mockup data for courses if service is down or not found
+    private fun getMockCourses(): ArrayList<Course> {
+        val courses = ArrayList<Course>()
+        courses.add(
+            Course(
+                "ANDR1",
+                "2021-09-19T14:30:16.450Z",
+                "2021-09-19T16:00:16.450Z",
+                "r10_2.41_t",
+                "BOOP02",
+            )
+        )
+        courses.add(
+            Course(
+                "TCI",
+                "2021-09-19T14:30:16.450Z",
+                "2021-09-19T16:00:16.450Z",
+                "r10_2.41_t",
+                "BOOP02",
+            )
+        )
+        courses.add(
+            Course(
+                "SOT",
+                "2021-09-19T14:30:16.450Z",
+                "2021-09-19T16:00:16.450Z",
+                "r10_2.41_t",
+                "BOOP02",
+            )
+        )
+        courses.add(
+            Course(
+                "PROEP",
+                "2021-09-19T14:30:16.450Z",
+                "2021-09-19T16:00:16.450Z",
+                "r10_2.41_t",
+                "BOOP02",
+            )
+        )
+        courses.add(
+            Course(
+                "WEBPRO",
+                "2021-09-19T14:30:16.450Z",
+                "2021-09-19T16:00:16.450Z",
+                "r10_2.41_t",
+                "BOOP02",
+            )
+        )
+        return courses
+    }
 }
