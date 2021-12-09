fd979227ea855637d75a57b91a895a778821e018
Justice Dreischor <t-justice98@hotmail.com>
2021-12-09 12:01:32 +0100

    fragment template cleanup

--- app/src/main/java/com/example/fhict_companion_app_group1/fragments/ProfileFragment.kt
+++ app/src/main/java/com/example/fhict_companion_app_group1/fragments/ProfileFragment.kt
@@ -1,5 +1,7 @@
 package com.example.fhict_companion_app_group1.fragments
 
+import agency.tango.android.avatarview.views.AvatarView
+import agency.tango.android.avatarviewglide.GlideLoader
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
@@ -10,28 +12,16 @@ import android.widget.TextView
 import com.example.fhict_companion_app_group1.R
 import com.example.fhict_companion_app_group1.activities.MainActivity
 import com.example.fhict_companion_app_group1.services.FHICTService
-import de.hdodenhof.circleimageview.CircleImageView
 import org.jetbrains.anko.doAsync
 import org.jetbrains.anko.uiThread
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [ProfileFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class ProfileFragment : Fragment() {
-    // TODO: Rename and change types of parameters
     private lateinit var fhictService: FHICTService
     private lateinit var profileFragmentView: View
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
         fhictService = (activity as MainActivity).getFhictService()
         profileFragmentView = inflater.inflate(R.layout.fragment_profile, container, false)
@@ -39,37 +29,21 @@ class ProfileFragment : Fragment() {
         return profileFragmentView
     }
 
-    private fun getInfo()
-    {
+    private fun getInfo() {
         doAsync {
             val me = fhictService.getMyInfo()
-            Log.d("TAG", me.displayName)
-            uiThread{
+            uiThread {
                 profileFragmentView.findViewById<TextView>(R.id.myProfileName).text = me.displayName
-                profileFragmentView.findViewById<TextView>(R.id.myProfileId).text = "I number: " + me.id
-                profileFragmentView.findViewById<TextView>(R.id.myProfileEmail).text = "Email: " + me.mail
-                profileFragmentView.findViewById<TextView>(R.id.myProfileDepartment).text = "Stream: " + me.department
-                Log.d("TAG", me.displayName)
+                profileFragmentView.findViewById<TextView>(R.id.myProfileId).text =
+                    getString(R.string.student_number).plus(me.id)
+                profileFragmentView.findViewById<TextView>(R.id.myProfileEmail).text =
+                    getString(R.string.email).plus(me.mail)
+                profileFragmentView.findViewById<TextView>(R.id.myProfileDepartment).text =
+                    getString(R.string.stream).plus(me.department)
+                val avatarView = profileFragmentView.findViewById(R.id.avatarView) as AvatarView
+                val imageLoader = GlideLoader()
+                imageLoader.loadImage(avatarView, "http:/example.com/user/someUserAvatar.png", me.displayName)
             }
         }
-    }
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment ProfileFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            ProfileFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
     }
 }
--- app/src/main/java/com/example/fhict_companion_app_group1/fragments/ScheduleFramgent.kt
+++ app/src/main/java/com/example/fhict_companion_app_group1/fragments/ScheduleFramgent.kt
@@ -1,12 +1,10 @@
 package com.example.fhict_companion_app_group1.fragments
 
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.fhict_companion_app_group1.R
@@ -14,10 +12,12 @@ import com.example.fhict_companion_app_group1.activities.MainActivity
 import com.example.fhict_companion_app_group1.adapters.CourseAdapter
 import com.example.fhict_companion_app_group1.models.Schedule
 import com.example.fhict_companion_app_group1.services.FHICTService
-import kotlinx.coroutines.delay
 import org.jetbrains.anko.doAsync
 import org.jetbrains.anko.uiThread
-import java.lang.Thread.sleep
+
+import agency.tango.android.avatarview.views.AvatarView
+import agency.tango.android.avatarviewglide.GlideLoader
+
 
 class ScheduleFragment : Fragment() {
     private val schedule = Schedule()
@@ -27,11 +27,12 @@ class ScheduleFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         fhictService = (activity as MainActivity).getFhictService()
         // Inflate the layout for this fragment
         scheduleFragmentView = inflater.inflate(R.layout.fragment_schedule, container, false)
         setCourses()
+
         return scheduleFragmentView
     }
 
